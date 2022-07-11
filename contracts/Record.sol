a1065d527e83fe26fdcc82f4274383710474152e
Susheel Thapa <susheel.thapa@outlook.com>
2022-07-12 00:39:08 +0545

    Create Patient Data type and method to add the patient to the Contracts

--- contracts/Record.sol
+++ contracts/Record.sol
@@ -3,7 +3,8 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Token {
     address public owner;
-    uint256 public total_doctor = 0;
+    uint256 public total_doctors = 0;
+    uint256 public total_patients = 0;
 
     constructor() {
         owner = msg.sender;
@@ -33,14 +34,19 @@ contract Token {
         return doctors[doctor_address].registered;
     }
 
+    function registerDoctor(address doctor_address) public onlyOwner {
+        doctors[doctor_address].registered = true;
+    }
+
     function addDoctor(
+        address doctor_address,
         string memory first_name,
         string memory last_name,
         uint8 age,
         string memory gender,
         string memory department
-    ) external {
-        doctors[msg.sender] = Doctor(
+    ) external onlyOwner {
+        doctors[doctor_address] = Doctor(
             first_name,
             last_name,
             age,
@@ -48,10 +54,42 @@ contract Token {
             department,
             false
         );
-        total_doctor += 1;
+        total_doctors += 1;
     }
 
-    function registerDoctor(address doctor_address) public onlyOwner {
-        doctors[doctor_address].registered = true;
+    enum PatientDiagnosisStatus {
+        not_diagnose,
+        diagnosed,
+        diagnosing
+    }
+
+    struct Patient {
+        string first_name;
+        string last_name;
+        uint8 age;
+        string gender;
+        uint8 total_medical_checkup;
+        PatientDiagnosisStatus status;
+    }
+
+    mapping(address => Patient) public patients;
+
+    function addPatient(
+        address patient_address,
+        string memory first_name,
+        string memory last_name,
+        uint8 age,
+        string memory gender
+    ) external onlyOwner {
+        patients[patient_address] = Patient(
+            first_name,
+            last_name,
+            age,
+            gender,
+            0,
+            PatientDiagnosisStatus.not_diagnose
+        );
+
+        total_patients += 1;
     }
 }
